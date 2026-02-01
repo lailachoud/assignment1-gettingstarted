3c719f5b9a92d919fdf057ee7dfef00bf30fe1fa
lailachoud <lc3957@nyu.edu>
2026-01-31 23:47:05 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -18,7 +18,7 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = 2   
+        answer = 1
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = 3
     else: 
@@ -34,6 +34,7 @@ if __name__ == "__main__":
     #use this space to debug and verify that the program works
     debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
+
 
 
 
